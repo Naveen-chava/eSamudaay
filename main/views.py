35e2dd8846bc69cf9430c086a0161727727d509d
Naveen Kumar <naveenkumarchava0@gmail.com>
2021-08-04 20:06:19 +0530

    fixed bug

--- main/views.py
+++ main/views.py
@@ -1,22 +1,32 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 @api_view(['POST'])
 def order_value(request):
     """
-    This function calculates the total order value based on items, distance and offer.
+    This function calculates the total order value based on itemsquantity, price, distance and offer.
     """
     request_data = request.data
 
-    check_fields(request_data)
-    
-    items_cost = 0
-    offer_val = 0
+    status = check_fields(request_data)
+    if status == False:
+        response = {
+                "status": False,
+                "message": "Mandatory fields missing!"
+        }
+        return Response(response)
 
     # items cost
+    items_cost = 0
     for i in request_data['order_items']:
         items_cost += i['quantity'] * i['price']
+    
+    if items_cost == 0:
+        response = {
+            "status": False,
+            "message": "Invalid Quantity!"
+        }
+        return Response(response)
 
     # delivery cost
     distance = request_data['distance']
@@ -30,6 +40,7 @@ def order_value(request):
     delivery_cost = get_delivery_cost(distance)
 
     # offer value
+    offer_val = 0
     if 'offer' in request_data:
         if request_data['offer']['offer_type'] == 'FLAT':
             offer_val = request_data['offer']['offer_val']
@@ -45,10 +56,11 @@ def order_value(request):
         if offer_val > items_cost:
             response = {
                 "status": False,
-                "message": "Offer value is greater than order value!"
+                "message": "Offer value is greater than the order value!"
             }
             return Response(response)
 
+    # total order value
     order_value = items_cost + delivery_cost - offer_val
 
     response = {
@@ -59,10 +71,10 @@ def order_value(request):
 
 def get_delivery_cost(distance):
     """
-    This function calculates the delivery cost based on the distance. The distance is in meters and the cost should be in paisa.
+    This function returns the delivery cost based on the distance. The distance is in meters and the cost is in paisa.
     """
     delivery_cost = 0 
-    if 0 < distance <= 10000:
+    if 0 <= distance <= 10000:
         delivery_cost = 5000
     elif 10000 < distance <= 20000:
         delivery_cost = 10000
@@ -75,18 +87,10 @@ def get_delivery_cost(distance):
 
 def check_fields(request_data):
     """
-    This function checks if any mandatory fields are missing. If any mandatory fields are missing, it returns a response.
+    This function checks if any mandatory fields are missing. If any mandatory fields are missing, it returns an error response.
     """
     mandatory_fields = ['order_items', 'distance']
     
     for field in mandatory_fields:
         if field not in request_data:
-            response = {
-                "status": False,
-                "error": {
-                    "code": 400,
-                    "message": "mandatory fields missing"
-                }
-            }
-        
-            return Response(response)
+            return False
